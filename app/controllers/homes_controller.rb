f42cadb2a5601583ddff054ca646e72dd9c2d684
yuuya0061 <258228254+yuuya0061@ users.noreply.github.com>
2026-06-20 15:24:35 +0000

    start

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,5 @@
+class HomesController < ApplicationController
+   allow_unauthenticated_access
+  def top
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root "homes#top"
+  resource :session
+  resources :passwords, param: :token
+
+  get "log_in", to: "sessions#new"
+  post "log_in", to: "sessions#create"
+  delete "log_out", to: "sessions#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
