1eb63097ee17a223446326755bb63519ef1d6565
yuuya0061 <258228254+yuuya0061@ users.noreply.github.com>
2026-06-23 07:50:47 +0000

    新規登録画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      start_new_session_for @user
+      redirect_to root_path, notice: "新規登録完了！！"
+    else
+      render :new,status: :unprocessable_entity
+    end
   end
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email_address, :password, :password_confirmation)
+  end
+  
 end
+
