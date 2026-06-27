b9b58237cdff0a69b7d4c9395c0ad51ba0f094f7
yuuya0061 <258228254+yuuya0061@ users.noreply.github.com>
2026-06-27 14:36:56 +0000

    編集画面途中

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,26 @@ class PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = Current.user.id
+    if @post.save
+      redirect_to posts_path, notice:"投稿しました！"
+    else
+      render :new,status: :unprocessable_entity
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
@@ -21,4 +32,9 @@ class PostsController < ApplicationController
   def destroy
   end
   
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+  
   validates :title, presence: true
   validates :body, presence: true
 end
