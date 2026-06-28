8218ebf550ab1f5a29449fcdd8b02e1a04eca1c8
yuuya0061 <258228254+yuuya0061@ users.noreply.github.com>
2026-06-28 13:41:42 +0000

    削除機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def new
     @post = Post.new
   end
@@ -27,14 +29,28 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "投稿更新しました" 
+    else 
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました"
   end
   
   private
 
   def post_params
     params.require(:post).permit(:title, :body, :image)
+  end
+
+  def correct_user
+    @post = Post.find(params[:id])
+    redirect_to posts_path if @post.user != Current.user
   end
 end
