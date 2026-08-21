ead55d2e23d648076c06c295d15023e60c8950c0
yuuya0061 <258228254+yuuya0061@ users.noreply.github.com>
2026-08-21 13:40:49 +0000

    実装フェーズ3完了

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,13 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+ validate :cannot_follow_yourself
+
+  def cannot_follow_yourself
+    if follower_id == followed_id
+      errors.add(:followed_id, "自分自身をフォローできません")if follower_id == followed_id
+    end
+  end
+
 end
